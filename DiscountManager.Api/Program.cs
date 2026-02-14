3f96dddd55c3c5deeebb71ffc5b571cd33c285d2
shervinfadaee@yahoo.com <shervinfadaee7900@gmail.com>
2026-02-14 17:18:54 +0100

    feat: Implement initial IdentityServer and ASP.NET Core Identity setup for user authentication.

--- DiscountManager.Api/Program.cs
+++ DiscountManager.Api/Program.cs
@@ -1,23 +1,80 @@
+using DiscountManager.Api.Data;
+using DiscountManager.Api.Extensions;
+using Duende.IdentityServer.EntityFramework.Mappers;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") 
+    ?? "Server=(localdb)\\mssqllocaldb;Database=DiscountManager;Trusted_Connection=True;MultipleActiveResultSets=true";
+var redisConnectionString = builder.Configuration.GetConnectionString("Redis") ?? "localhost:6379";
+
+builder.Services.AddDatabaseContext(connectionString);
+builder.Services.AddIdentityAndIdentityServer();
+builder.Services.AddAdvancedCaching(redisConnectionString);
+builder.Services.AddSystematicHealthChecks(connectionString, redisConnectionString);
 
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
 
+app.UseIdentityServer();
+app.UseAuthentication();
 app.UseAuthorization();
 
+app.MapHealthChecks("/health", new Microsoft.AspNetCore.Diagnostics.HealthChecks.HealthCheckOptions
+{
+    ResponseWriter = HealthChecks.UI.Client.UIResponseWriter.WriteHealthCheckUIResponse
+});
+
 app.MapControllers();
+
+// Seed IdentityServer data
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    var context = services.GetRequiredService<ApplicationDbContext>();
+    context.Database.EnsureCreated();
+
+    if (!context.Clients.Any())
+    {
+        foreach (var client in DiscountManager.Api.Data.Config.Clients)
+        {
+            context.Clients.Add(client.ToEntity());
+        }
+        context.SaveChanges();
+    }
+
+    if (!context.IdentityResources.Any())
+    {
+        foreach (var resource in DiscountManager.Api.Data.Config.IdentityResources)
+        {
+            context.IdentityResources.Add(resource.ToEntity());
+        }
+        context.SaveChanges();
+    }
+
+    if (!context.ApiScopes.Any())
+    {
+        foreach (var scopeItem in DiscountManager.Api.Data.Config.ApiScopes)
+        {
+            context.ApiScopes.Add(scopeItem.ToEntity());
+        }
+        context.SaveChanges();
+    }
+}
 
 app.Run();
